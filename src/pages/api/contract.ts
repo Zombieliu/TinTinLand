be69a61f68f2ec0970b7741f265898d0c24e3c9f
henryliu <17600047539@163.com>
2023-03-03 01:57:18 +0800

    update

---
+++ src/pages/api/contract.ts
@@ -0,0 +1,10 @@
+import  contractUri from './json/tintin.json'
+
+export default async function handler(req, res) {
+    try {
+        // const result = {hello:1}
+        res.status(200).json({ contractUri })
+    } catch (err) {
+        res.status(500).json({ error: 'failed to load data' })
+    }
+}
